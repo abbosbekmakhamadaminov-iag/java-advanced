209bcc68b9674481d77fdab69de1fe695fe90077
Abbosbek Makhamadaminov <abbosbek.makhamadaminov@alertinnovation.com>
2025-05-16 17:34:31 +0500

    First task: Implement CommandLineRunner hello world

---
+++ src/main/java/com/epam/javadvanced/JavadvancedApplication.java
@@ -0,0 +1,20 @@
+package com.epam.javadvanced;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JavadvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavadvancedApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> System.out.println("Hello world");
+	}
+
+}
---
+++ src/test/java/com/epam/javadvanced/JavadvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.javadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavadvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
